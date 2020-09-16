58d02ca503f3b8741782ee8759d032160e97de83
DeanLaz <DeanLazx@gmail.com>
2020-09-16 13:57:51 -0700

    Yargs Package

--- NotesX/app.js
+++ NotesX/app.js
@@ -1,6 +1,46 @@
 const validator = require("validator");
+const yargs = require("yargs");
 const getNotes = require("./notes.js");
 const chalk = require("chalk");
 
-console.log(chalk.bold.green("Chalk in Green"));
-console.log(validator.isEmail("deanlazx@gmail.com"));
+console.log(yargs.argv);
+
+// ADD COMMAND
+yargs.command({
+  command: "add",
+  describe: "Add a new Note",
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: (argv) => {
+    console.log("Adding new note");
+  },
+});
+
+yargs.command({
+  command: "remove",
+  describe: "Removed a new Note",
+  handler: (argv) => {
+    console.log("Removing new note");
+  },
+});
+
+yargs.command({
+  command: "list",
+  describe: "Listing out all Notes",
+  handler: () => {
+    console.log("Listing the Note");
+  },
+});
+
+yargs.command({
+  command: "read",
+  describe: "Reading the Note",
+  handler: (argv) => {
+    console.log("Reading the Note");
+  },
+});
