96cca199f6d7a754aefdc5b063365e7fe40aa398
DeanLaz <DeanLazx@gmail.com>
2020-09-17 19:15:51 -0700

    MongoDB

---
+++ TaskManagerX/mongodb.js
@@ -0,0 +1,60 @@
+const { MongoClient, ObjectID } = require("mongodb");
+
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "taskmanagerx";
+
+// const id = new ObjectID();
+// console.log(id);
+// console.log(id.getTimestamp);
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return console.log("Unable to connect to database!");
+    }
+
+    // const db = client.db(databaseName);
+    // db.collection("users").insertOne(
+    //   {
+    //     _id: id,
+    //     name: "Dean Lazarof",
+    //     age: 22,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert user");
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Dean',
+    //         age: 28
+    //     }, {
+    //         name: 'Josh',
+    //         age: 22
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents!')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+    // db.collection("tasks")
+    //   .deleteOne({
+    //     description: "Clean the house",
+    //   })
+    //   .then((result) => {
+    //     console.log(result);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+  }
+);
